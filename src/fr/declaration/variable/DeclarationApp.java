de4f15805b9ad0ec64d7e83e01f19350a90c5edf
Teixeira <teixeirac.jordan@gmail.com>
2020-06-23 12:21:24 +0200

    first commit

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,19 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+	public static void main(String[] args) {
+		byte var1 = 12;
+		short var2 = 20000;
+		int var3 = 100000;
+		long var4 = 1000000;
+		float var5 = 2.5f;
+		double var6 = 2.55d;
+		char var7 = 'f';
+		boolean var8 = true;
+		String var9 = "hello";
+		
+		System.out.println(var6);
+		System.out.println("Salut à tous");
+		
+	}
+}
